2632601c093dc8e7861a9d054cf8db0bde320160
Ricky Barfield <barfieldfredrick@gmail.com>
2024-01-09 19:54:21 -0700

    trying two word palingrams tomorrow

---
+++ palingram_spells/two_word_palingrams.py
@@ -0,0 +1,7 @@
+'''Objective: Use Python to search an english dict for two word
+palingrams. Analyze and optimize the palingram code using the cProfile
+tool
+'''
+
+def two_word_palingram(wordlist):
+    '''main funct'''
